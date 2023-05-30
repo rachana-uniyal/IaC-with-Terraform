e4befb70796d6fac6b84d1fe5bf6e095d136a35c
rachana-uniyal <rachanauniyal0417@gmail.com>
2023-05-30 15:50:45 +0530

    ansible configuration

--- main.tf
+++ main.tf
@@ -1,52 +1,156 @@
 provider "aws" {
-    region = "eu-west-3"
+  region = "eu-west-3"
 }
 
-variable cidr_blocks {
-    description = "cidr blocks and name tags for vpc and subnets"
-    type = list(object({
-        cidr_block = string
-        name = string
-    }))
+variable vpc_cidr_block {}
+variable subnet_1_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+variable instance_type {}
+variable ssh_key {}
+variable my_ip {}
+variable ssh_key_private {}
+
+data "aws_ami" "amazon-linux-image" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
-variable avail_zone {
-    default = "eu-west-3a"
+output "ami_id" {
+  value = data.aws_ami.amazon-linux-image.id
 }
 
 resource "aws_vpc" "myapp-vpc" {
-    cidr_block = var.cidr_blocks[0].cidr_block
-    tags = {
-        Name = var.cidr_blocks[0].name
-    }
+  cidr_block = var.vpc_cidr_block
+  enable_dns_hostnames = true
+  tags = {
+      Name = "${var.env_prefix}-vpc"
+  }
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    cidr_block = var.cidr_blocks[1].cidr_block
-    availability_zone = var.avail_zone
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_1_cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+      Name = "${var.env_prefix}-subnet-1"
+  }
+}
+
+resource "aws_security_group" "myapp-sg" {
+  name   = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-sg"
+  }
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+	vpc_id = aws_vpc.myapp-vpc.id
+    
     tags = {
-        Name = var.cidr_blocks[1].name
-    }
+     Name = "${var.env_prefix}-internet-gateway"
+   }
 }
 
-output "dev-vpc-id" {
-    value = aws_vpc.development-vpc.id
+resource "aws_route_table" "myapp-route-table" {
+   vpc_id = aws_vpc.myapp-vpc.id
+
+   route {
+     cidr_block = "0.0.0.0/0"
+     gateway_id = aws_internet_gateway.myapp-igw.id
+   }
+
+   # default route, mapping VPC CIDR block to "local", created implicitly and cannot be specified.
+
+   tags = {
+     Name = "${var.env_prefix}-route-table-test"
+   }
+ }
+
+# Associate subnet with Route Table
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id      = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
 }
 
-output "dev-subnet-id" {
-    value = aws_subnet.dev-subnet-1.id
+resource "aws_key_pair" "ssh-key" {
+  key_name   = "myapp-key"
+  public_key = file(var.ssh_key)
 }
 
-data "aws_vpc" "existing_vpc" {
-    default = true
+output "server-ip" {
+    value = aws_instance.myapp-server.public_ip
 }
 
-resource "aws_subnet" "dev-subnet-2" {
-    vpc_id = data.aws_vpc.existing_vpc.id
-    cidr_block = "172.31.48.0/20"
-    availability_zone = "eu-west-3a"
-    tags = {
-        Name = "subnet-2-default"
-    }
+resource "aws_instance" "myapp-server" {
+  ami                         = data.aws_ami.amazon-linux-image.id
+  instance_type               = var.instance_type
+  key_name                    = "myapp-key"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids      = [aws_security_group.myapp-sg.id]
+  availability_zone			      = var.avail_zone
+
+  tags = {
+    Name = "${var.env_prefix}-server"
+  }
 }
+
+resource "aws_instance" "myapp-server-two" {
+  ami                         = data.aws_ami.amazon-linux-image.id
+  instance_type               = var.instance_type
+  key_name                    = "myapp-key"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids      = [aws_security_group.myapp-sg.id]
+  availability_zone			      = var.avail_zone
+
+  tags = {
+    Name = "${var.env_prefix}-server"
+  }
+
+   provisioner "local-exec" {
+    working_dir = "../ansible"
+    command = "ansible-playbook --inventory ${self.public_ip}, --private-key ${var.ssh_key_private} --user ec2-user deploy-docker-new-user.yaml"
+  } 
+}
+
+/* 
+resource "null_resource" "configure_server" {
+  triggers = {
+    trigger = aws_instance.myapp-server.public_ip
+  }
+
+  provisioner "local-exec" {
+    working_dir = "../ansible"
+    command = "ansible-playbook --inventory ${aws_instance.myapp-server.public_ip}, --private-key ${var.ssh_key_private} --user ec2-user deploy-docker-new-user.yaml"
+  }
+}
+*/
